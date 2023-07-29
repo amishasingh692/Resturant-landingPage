6f817517b59a40a24946a9bb4520de89037fee47
anu <amishasingh765@gmail.com>
2023-07-30 00:54:12 +0530

    first commit

---
+++ button.js
@@ -0,0 +1,15 @@
+function Buffet() {
+
+     var a = document.getElementById("buffet");
+     a.style.display = "block";
+     var b = document.getElementById("break");
+     b.style.display = "none";
+     var c = document.getElementById("lunch");
+     c.style.display = "none";
+     var d = document.getElementById("chef");
+     d.style.display = "none";
+     var e = document.getElementById("desert");
+     e.style.display = "none";
+     var f = document.getElementById("drink");
+     f.style.display = "none";
+   }
